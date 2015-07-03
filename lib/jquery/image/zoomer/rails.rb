24b447da97cfd854d114bc37166ceafaae840731
Sahidur Rahman Suman <suman5040@gmail.com>
2015-07-04 00:15:39 +0600

    first commit

---
+++ lib/jquery/image/zoomer/rails.rb
@@ -0,0 +1,11 @@
+require "jquery/image/zoomer/rails/version"
+
+module Jquery
+  module Image
+    module Zoomer
+      module Rails
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/jquery/image/zoomer/rails/version.rb
@@ -0,0 +1,9 @@
+module Jquery
+  module Image
+    module Zoomer
+      module Rails
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
